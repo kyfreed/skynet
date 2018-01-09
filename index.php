a11e5b5265f01d4f6df776949e7d60bcc801ddc5
Kyle <kyle.freed@icloud.com>
2018-01-09 14:00:45 -0800

    skynet

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<head>
+    <script src="jquery-3.2.1.js">
+    <script src="main.js">
+</head>
+<body>
+Seconds until Kyle implements Skynet:
+        <br>
+        <p id="secs"></p>
+</body>
---
+++ main.js
@@ -0,0 +1,11 @@
+setInterval(tick(), 1000);
+
+function tick(){
+    var now  = new Date();
+    var then = new Date("January 1, 2029 00:00:00");
+    var away = then - now;
+    away = Math.floor(away / 1000);
+    document.getElementById("secs").innerHTML = away;
+}
+
+
